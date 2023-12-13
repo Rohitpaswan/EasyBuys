28e559ff16c01e40f4e01d31453e92c647587249
Rohit Paswan <paswanrohit0207@gmail.com>
2023-12-13 14:57:05 +0530

    Updated routes in the application

--- src/App.jsx
+++ src/App.jsx
@@ -11,8 +11,8 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/products" element={<ProductListingPage />} />
-        <Route path="/cart" element={<Cart />} />
+        <Route path="EasyBuys/products" element={<ProductListingPage />} />
+        <Route path="EasyBuys/cart" element={<Cart />} />
       </Routes>
     </div>
   );
