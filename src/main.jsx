638ce68fd99390d949b5c177bb565c4b8ccaa115
Rohit Paswan <paswanrohit0207@gmail.com>
2023-12-17 15:49:22 +0530

    update package , app , main

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import "./index.css";
 import ProductContextProvider from "./context/productContext/ProductContextProvider.jsx";
 import CartContextProvider from "./context/cartContext/CartContextProvider.jsx";
 import { BrowserRouter } from "react-router-dom";
