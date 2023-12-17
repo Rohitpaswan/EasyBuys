50ca5bd44c3e42b2f476d9f5b68e4704270bf4d8
Rohit Paswan <paswanrohit0207@gmail.com>
2023-12-18 00:30:34 +0530

    test product page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import Home from "./pages/Home/Home";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
 import ProductListingPage from "./pages/ProductListing/ProductListingPage";
 import Cart from "./pages/Cart/Cart";
 import Navbar from "./components/navbar/Navbar";
@@ -9,6 +9,11 @@ function App() {
   return (
     <div className="main">
       <Navbar />
+     <Link to="./products">
+     <button style={{padding:'10px' ,margin:'16px'}}>Click me for products page</button>
+     </Link>
+     
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/products" element={<ProductListingPage />} />
