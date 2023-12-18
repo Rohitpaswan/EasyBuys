8b790c05b9599f48a7a5aeb58e4e7238352b768b
Rohit Paswan <paswanrohit0207@gmail.com>
2023-12-18 18:04:19 +0530

    test for product page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,13 @@
+import { Link } from "react-router-dom"
 
 const Home = () => {
   return(
     <>
 
-      
+      <Link to='./products'>
+      <button >Click for product page</button>
+      </Link>
+
 
       
     </>
